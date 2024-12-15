c70cdd1d92b88b67944fd2c366f7b7116050f3a0
Raghavendra <nraghavendra89@gmail.com>
2024-12-15 16:56:44 +0530

    Ability to input the code. - Allow only one letter per box. - On click backspace or delete, delete the letter and focus back to previous box.

--- src/CodeInput.ts
+++ src/CodeInput.ts
@@ -3,7 +3,6 @@ type CodeInputOptions = {
 }
 
 interface renderable {
-    html: string,
     render(): void,
 }
 
@@ -24,11 +23,13 @@ const boxStyles: Styles = {
     'display': 'inline-flex',
     'justify-content': 'center',
     'align-items': 'center',
-    'font-size': '2rem'
+    'font-size': '2rem',
+    'caret-color': 'transparent'
 };
 
 class CodeInput implements renderable {
-    html: string;
+    private html: string;
+    private inputBoxes: HTMLCollection;
 
     element: HTMLElement;
     options: CodeInputOptions = {
@@ -67,7 +68,64 @@ class CodeInput implements renderable {
         this.html += '</div>'
     }
 
+    private _moveToBox: (fromBox: HTMLElement, toBox: HTMLElement) => void = (fromBox, toBox) => {
+        if (toBox) {
+            // Do we need to remove the contenteditable?
+            fromBox.setAttribute('contenteditable', 'false');
+
+            toBox.setAttribute('contenteditable', 'plaintext-only');
+
+            toBox.focus();
+        }
+    }
+
+    private _keyDownHandler: (e: KeyboardEvent) => void = (e) => {
+        let thisInputBox = e.target as HTMLElement;
+
+        if (thisInputBox.textContent.length) {
+            // Allow only one input
+            e.preventDefault();
+            return;
+        }
+
+
+        console.log('Key down...');
+    }
+
+    private _keyUpHandler: (e: KeyboardEvent) => void = (e) => {
+        let thisInputBox = e.target as HTMLElement;
+
+        // If it is back or delete key delete the entry
+        if (e.key == 'Backspace' || e.key == 'Delete') {
+            thisInputBox.innerText = '';
+
+            // Move to previous box
+            let prevInputBox = thisInputBox.previousElementSibling as HTMLInputElement;
+            this._moveToBox(thisInputBox, prevInputBox);
+
+            return;
+        }
+
+        if (thisInputBox.textContent.length) {
+            // Move to next box
+            let nextInputBox = thisInputBox.nextElementSibling as HTMLInputElement;
+            this._moveToBox(thisInputBox, nextInputBox);
+        }
+    }
+
+    private _attachEventListeners: () => void = () => {
+
+        for (var i = 0; i < this.inputBoxes.length; i++) {
+            this.inputBoxes[i].addEventListener('keydown', this._keyDownHandler, false);
+            this.inputBoxes[i].addEventListener('keyup', this._keyUpHandler, false);
+        }
+    }
+
     render: () => void = () => {
         this.element.innerHTML = this.html;
+
+        this.inputBoxes = this.element.querySelector('div').children;
+
+        this._attachEventListeners();
     }
 }
